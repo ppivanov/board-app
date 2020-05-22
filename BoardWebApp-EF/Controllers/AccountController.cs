3fadde49ec3a3683683203125c719af577cd5f91
Pavel Ivanov <ppivanov98@gmail.com>
2020-05-22 21:39:42 +0100

    Registration validations are kind of working

---
+++ BoardWebApp-EF/Controllers/AccountController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BoardWebApp.Models;
+using BoardWebApp.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BoardWebApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        private BoardWebAppContext _dbContext;
+        public AccountController(BoardWebAppContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        // GET: /<controller>/
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(UserRegistrationModel newUserValues)
+        {
+            if (EmailBelongsToAnotherUser(newUserValues.Email))
+                Console.WriteLine("Email " + newUserValues.Email + " already in use");
+            else
+                Console.WriteLine("Email is all good!!");
+
+            //TODO
+            return View();
+        }
+
+        public bool EmailBelongsToAnotherUser(string value)
+        {
+            var EmailStore = _dbContext.User.Where(user => user.Email == value);
+            var userFromQuery = EmailStore.FirstOrDefault();
+            //If an email is already acossiated with another user 
+            if (userFromQuery != null)
+            {
+                Console.WriteLine("userFromQuery's Email = " + userFromQuery.Email);
+                return true;
+            }
+            // else the email is not related to any users and is free to use
+            else
+                return false;
+        }
+    }
+}
---
+++ BoardWebApp-EF/ViewModels/UserRegistrationModel.cs
@@ -0,0 +1,40 @@
+﻿using BoardWebApp.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BoardWebApp.ViewModels
+{
+    public class UserRegistrationModel
+    {
+        [Display(Name = "First name")]
+        [Required]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Last name")]
+        [Required]
+        public string LastName { get; set; }
+
+        //private string _Email;
+
+        [Display(Name = "Email address")]
+        [Required]
+        [EmailAddress]
+        public string Email { get; set;
+        }
+
+        [Required(ErrorMessage = "Password is required")]
+        [DataType(DataType.Password)]
+        [RegularExpression(@"(^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,32}$)?(^(?=.*\d)(?=.*[a-z])(?=.*[@#$%^&+=]).{8,32}$)?(^(?=.*\d)(?=.*[A-Z])(?=.*[@#$%^&+=]).{8,32}$)?(^(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=]).{8,32}$)?", ErrorMessage = "Password must be 8 to 32 characters long and must contain at least:<br/>- 1 lower case letter<br/>- 1 upper case letter<br/>- 1 numeric character<br/>- 1 special character")]
+        public string Password { get; set; }
+
+        [Display(Name = "Confirm password")]
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare("Password")]
+        public string ConfirmPassword { get; set; }
+    }
+}
