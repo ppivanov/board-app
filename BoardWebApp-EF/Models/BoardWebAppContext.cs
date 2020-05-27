f29c0ac9a1afedabf5cc925f1d454f8ebf904904
Pavel Ivanov <ppivanov98@gmail.com>
2020-05-27 23:57:12 +0100

    Commented out the implicit database connection section in BoardWebAppContext. That connection is injected via services in Startup.cs

--- BoardWebApp-EF/Models/BoardWebAppContext.cs
+++ BoardWebApp-EF/Models/BoardWebAppContext.cs
@@ -32,8 +32,8 @@ namespace BoardWebApp.Models
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=BoardWebApp;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+//                optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=BoardWebApp;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
             }
         }
 
