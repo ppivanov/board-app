bdb864e0af14a767241c1adcfd152f308041f8ee
Pavel Ivanov <ppivanov98@gmail.com>
2020-06-06 23:32:52 +0100

    missed a file in last commit

--- BoardWebApp-Tests/RegistrationValidationTests.cs
+++ BoardWebApp-Tests/RegistrationValidationTests.cs
@@ -67,6 +67,7 @@ namespace BoardWebApp_Tests
                         LastName = "Yu",
                         Password = "00fcdde26dd77af7858a52e3913e6f3330a32b3121a61bce915cc6145fc44453",
                         EmailHash = "e8107daab70f9bec25d249541eb247f36514248a14597b3cdc5ebaa3bb140a68"
+                        // cookie value = e8107daab70f9bec25d249541eb247f36514248a14597b3cdc5ebaa3bb140a68c472a2c6627ceb7c0f1b1f1c5e8ed5a903fcede83b4b9da720697c3dc127ffff
                     }, new User
                     {
                         Email = "trudy@board.com",
@@ -74,8 +75,26 @@ namespace BoardWebApp_Tests
                         LastName = "Turner",
                         Password = "00fcdde26dd77af7858a52e3913e6f3330a32b3121a61bce915cc6145fc44453",
                         EmailHash = "08176ba0671827d033a25cfa6608d92caca8008527af3ff32dc23012dc99d554"
+                        // cookie value = 08176ba0671827d033a25cfa6608d92caca8008527af3ff32dc23012dc99d554217161eda88a0d393461c215f94cdbfb787e82ed7c8e5db2a2bfcbea69c7a0d4
+                    }, new User
+                    {
+                        Email = "alicia@board.com",
+                        FirstName = "Alicia",
+                        LastName = "Keys",
+                        Password = "00fcdde26dd77af7858a52e3913e6f3330a32b3121a61bce915cc6145fc44453",
+                        EmailHash = "ed0f7a304e3f1337db52d0e5f3087cd02ef4e16f4665274b23f7b423f56b4180"
+                        // cookie value = ed0f7a304e3f1337db52d0e5f3087cd02ef4e16f4665274b23f7b423f56b4180e0860fdd8998c527f848820d1d0be2b07b42c0a8c54ba49a31f70888a436daad
                     }
                 );
+                dbContext.Project.AddRange(
+                    new Project
+                    {
+                        ProjectId = 1,
+                        ProjectName = 
+                    }
+                );
+
+
             dbContext.SaveChanges();
             return dbContext;
         }
